ecfece9de8c31de84bd4b9234692af0d347b54d0
SunLightJuly <winder99@tom.com>
2013-11-27 18:51:25 +0800

    test update

---
+++ temp/compile_zip/prj_compile_scripts.bat
@@ -0,0 +1,2 @@
+call %QUICK_X_BIN%\compile_scripts.bat -i scripts -o scripts_zip\game.zip -m zip
+call %QUICK_X_BIN%\compile_scripts.bat -i scripts -o scripts_zip\game.bin -m zip -e xxtea_zip -ek _key_ddz_gw_ -es gwsoft
