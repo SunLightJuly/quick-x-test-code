66b037359d6d3bc2123b867e14e78b8a140ac3ca
SunLightJuly <winder99@tom.com>
2014-05-19 00:41:35 +0800

    test cclass

--- hello223/scripts/cclass.lua
+++ hello223/scripts/cclass.lua
@@ -1,8 +1,12 @@
 
 function CClass(classname)
 	-- check if classname has been declared
-	ALL_CCLASS_REG = ALL_CCLASS_REG or {}
+	-- ALL_CCLASS_REG = ALL_CCLASS_REG or {}
 	local mt = ALL_CCLASS_REG[classname]
+	-- print("mt="..mt)
 	if (mt~=nil) then return end
 
+	local s = lua_binder.load(classname)
+	print(s)
+	lua_binder.load(nil)
 end
