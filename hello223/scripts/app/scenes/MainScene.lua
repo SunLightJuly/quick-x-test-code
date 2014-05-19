bb84096b462a7cd303d1fafa1efbaceda5e6e91b
SunLightJuly <ygqy8181@qq.com>
2014-05-19 18:13:35 +0800

    create cfunc list table ok

--- hello223/scripts/app/scenes/MainScene.lua
+++ hello223/scripts/app/scenes/MainScene.lua
@@ -8,6 +8,7 @@ CClass("CCNodeTest")
 
 function MainScene:ctor()
 	-- local node_t = CCNodeTest:create()
+	print(CCNodeTest.test("test string!!!"))
 
     ui.newTTFLabel({text = "Hello, 世界", size = 64, align = ui.TEXT_ALIGN_CENTER})
         :pos(display.cx, display.cy)
--- hello223/scripts/cclass.lua
+++ hello223/scripts/cclass.lua
@@ -1,12 +1,24 @@
 
 function CClass(classname)
 	-- check if classname has been declared
-	-- ALL_CCLASS_REG = ALL_CCLASS_REG or {}
+	ALL_CCLASS_REG = ALL_CCLASS_REG or {}
 	local mt = ALL_CCLASS_REG[classname]
-	-- print("mt="..mt)
 	if (mt~=nil) then return end
 
-	local s = lua_binder.load(classname)
-	print(s)
-	lua_binder.load(nil)
+	dump(QUICKX_CCLASS_REG_FUNC)
+
+	mt = {}
+	mt.__index, err = lua_binder.load(classname, {})
+	if (mt.__index==nil) then
+		print("CClass Error: "..err)
+		-- return
+	end
+	dump(mt.__index)
+
+	local cls = {}
+	setmetatable(cls, mt)
+	rawset(_G, classname, cls)
+	dump(_G[classname])
+
+	ALL_CCLASS_REG[classname] = mt
 end
