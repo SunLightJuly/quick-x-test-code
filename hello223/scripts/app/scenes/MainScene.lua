0195ce3a191e76c683a82ec60cbad7d946584535
SunLightJuly <ygqy8181@qq.com>
2014-05-22 17:50:20 +0800

    test work finish

--- hello223/scripts/app/scenes/MainScene.lua
+++ hello223/scripts/app/scenes/MainScene.lua
@@ -9,16 +9,48 @@ CClassNameSpace("ccx")
 cclass("CCNodeTest", cc, "NodeTest")
 cclass("CCSpriteTest", cc, "SpriteTest")
 
-function MainScene:ctor()
-	-- local node_t = CCNodeTest:create()
-	-- local sprite = cc.Sprite:create()
+function cc.SpriteTest:new(...)
+	return self.create(...)
+end
 
-	local sp = cc.SpriteTest:create()
-	dump(sp)
+function MainScene:ctor()
+	local x
+
+	local node = cc.Node:create()
+	x = os.clock()
+	for i=1,1000000 do
+		node:setPosition(100, 100)
+	end
+	print(string.format("elapsed time 1: %.2f\n", os.clock() - x))
+
+	local node_t = cc.NodeTest:create()
+	x = os.clock()
+	for i=1,1000000 do
+		node_t:setPosition(100, 100)
+	end
+	print(string.format("elapsed time 2: %.2f\n", os.clock() - x))
+
+	local sprite = cc.Sprite:create("helloworld.png")
+	x = os.clock()
+	for i=1,1000000 do
+		sprite:setPosition(100, 100)
+	end
+	print(string.format("elapsed time 3: %.2f\n", os.clock() - x))
+
+	local sp = cc.SpriteTest:new("helloworld.png")
+	x = os.clock()
+	for i=1,1000000 do
+		sp:setPosition(100, 100)
+	end
+	print(string.format("elapsed time 4: %.2f\n", os.clock() - x))
+
+	-- dump(sp)
+	sp.testValue = 10101010101
 	-- dump(getmetatable(sp))
 	print("####cclass_type_name: "..cclass_type_name(sp))
 	-- print(sp.udata_)
-	print(cc.SpriteTest:setPosition(10, 10).test("test cc.spritetest!!!"))
+	print(sp:setPosition(10, 10).test("test sp:test!!! --- ")..sp.testValue)
+	print(cc.SpriteTest.test("test cc.spritetest!!!"))
 
 	-- print(ccx.CCSpriteTest.test("test sprite!!!"))
 	-- print(ccx.CCNodeTest.test("test string!!!"))
--- hello223/scripts/cclass.lua
+++ hello223/scripts/cclass.lua
@@ -1,7 +1,7 @@
 
 local function getArrayType( c )
 	if c=="C" then
-		return type("")
+		return "string" --type("")
 	else
 		return "userdata"
 	end
@@ -16,14 +16,30 @@ local function getRetType( typeString )
 	if c=="[" then
 		return getArrayType( string.sub(typeString, 2, 2) )
 	elseif c=="Z" then
-		return type(true)
+		return "boolean"
 	elseif c=="I" then
-		return type(1)
+		return "number"
 	elseif c=="F" then
-		return type(1.0)
+		return "number"
 	end
 
 	return "cclass", string.sub(typeString, 2, -2)
+end
+
+-- if cls is a "cclass" and not a "cclass object" return true
+function cclass_is_class( cls )
+	if type(cls)~="table" then return false end
+	local mt = getmetatable(cls)
+	if not mt or not mt.classname_ then return false end
+	if cclass_is_obj(cls) then return false end
+	return true
+end
+
+-- if obj is a "cclass object" return true
+function cclass_is_obj( obj )
+	if not obj.udata_ then return false end
+
+	return true
 end
 
 local function wrapObj(udata, mt)
@@ -34,7 +50,7 @@ local function wrapObj(udata, mt)
 end
 
 local function convert_args( args, types )
-	dump(args)
+	-- dump(args)
 	local args_new = {nil}
 	if type(types)~="table" then return args_new end
 
@@ -42,10 +58,13 @@ local function convert_args( args, types )
 	local typeWant
 	local typeCur
 	for i,v in ipairs(types) do
+		-- local arg = args[i]
 		n = i
 		typeCur = cclass_type_name(args[i])
 		typeWant = v
-		if not cclass_type_compare( typeCur, typeWant, cc ) then 
+		if cclass_type_compare( typeCur, typeWant, cc ) then 
+			typeCur = typeWant
+		else
 			break 
 		end
 		if type(args[i])~=typeCur then
@@ -57,61 +76,109 @@ local function convert_args( args, types )
 		end
 	end	
 
-	dump(args)
+	-- dump(args)
 	if typeCur==typeWant then return args end
 
 	print("[CCLASS ERR] bad argument #"..n.."("..typeWant.." expected, got "..typeCur)
 end
 
-local function split_sign_string( str )
-	if not str or string.len(str)<1 then return true end
-	local args = string.split(str, '|')
-	dump(args)
+local function convert_args1( args )
 	for i,v in ipairs(args) do
-		if v=='F' or v=='I' or v=='J' then 
-			args[i] = type(1) 
-		elseif v=='T' then
-			args[i] = type("")
-		elseif v=='Z' then 
-			args[i] = type(true)
+		if type(v)=="table" and v.udata_ then
+			args[i] = v.udata_
 		end
 	end
-	dump(args)
+
 	return args
 end
 
+local function split_sign_string( str )
+	if not str or string.len(str)<1 then return true end
+	local args = string.split(str, '|')
+	-- dump(args)
+	for i,v in ipairs(args) do
+		if v=='F' or v=='I' or v=='J' then 
+			args[i] = "number" 
+		elseif v=='T' then
+			args[i] = "string"
+		elseif v=='Z' then 
+			args[i] = "boolean"
+		end
+	end
+	-- dump(args)
+	return args
+end
+
+local cclass_is_class_ = cclass_is_class
+local cclass_is_obj_ = cclass_is_obj
+
 local function call_cfunction_evt( mt, evt, evtname, ... )
-	print("call_cfunction_evt--------->")
-	dump(evt)
-	if not evt.mothodParamsType then
-		evt.mothodParamsType = split_sign_string(evt.methodSign)
+	-- print("call_cfunction_evt--------->")
+	-- dump(evt)
+	-- if not evt.mothodParamsType then
+	-- 	evt.mothodParamsType = split_sign_string(evt.methodSign)
+	-- end
+
+	local args = {...}
+	local self_obj = args[1]
+	-- if cclass_is_class_(self_obj) then
+	-- 	-- somebady should use "Class:Func"
+	-- 	-- remove the extra param
+	-- 	table.remove(args,1)
+	-- elseif not cclass_is_obj_(self_obj) then
+	-- 	self_obj = nil
+	-- end
+	if not cclass_is_obj_(self_obj) then
+		self_obj = nil
 	end
 
-	local args = convert_args( {...}, evt.mothodParamsType )
-	if not args then
-		print("[CCLASS ERR] Error in "..mt.classname_..":"..evtname)
-		return
-	end
-	dump(args)
+	-- args = convert_args( args, evt.mothodParamsType )
+	-- args = convert_args1( args )
+	-- if not args then
+	-- 	print("[CCLASS ERR] Error in "..mt.classname_..":"..evtname)
+	-- 	return
+	-- end
+	-- dump(args)
 
 	local retType = getRetType(evt.retType)
-	-- return void
+	-- if return void, return self object instead
 	if not retType then
-		evt.cfunc(unpack(args))
-		return
+		-- evt.cfunc(unpack(args))
+		evt.cfunc(...)
+		return self_obj
 	-- return c-object
 	elseif retType=="cclass" then
-		local udata = evt.cfunc(unpack(args))
+		-- local udata = evt.cfunc(unpack(args))
+		local udata = evt.cfunc(...)
 		return wrapObj(udata, mt)
 	end
 
 	-- return normal
-	return evt.cfunc(unpack(args))
+	-- return evt.cfunc(unpack(args))
+	return evt.cfunc(...)
+end
+
+function cclass_has_super( obj, supername )
+		local mt_ = getmetatable(obj)
+		if not mt_ then return false end
+		-- print("cclass_has_super("..mt_.classname_..","..supername..")")
+
+		for i,v in ipairs(mt_.cclass_super) do
+			-- print("cclass_has_super: v="..v)
+			if v==supername then return true end
+			local super_ = mt_.namespace_[v]
+			if cclass_has_super( super_, supername ) then
+				return true
+			end
+		end
+
+		return false
 end
 
 local function cclass_mt_index( target, keyname )
 		-- find function in metatable
 		local mt_ = getmetatable(target)
+
 		local evt = mt_[keyname]
 		if evt then
 			if type(evt)~="table" or not evt.cfunc then
@@ -171,19 +238,25 @@ function cclass(classname, namespace, aliasname)
 	end
 end
 
+-- param "cur" & "want" is string of class name
+-- param "ns" is namespace
+-- if cur==want or class(want) is super class of class(cur), return true
 function cclass_type_compare( cur, want, ns )
 	if cur==want then return true end
+	if cclass_has_super(ns[cur], want) then return true end
 
 	return false
 end
 
+-- if obj is cclass type, return it's real class name
+-- otherwise, return type(obj)
 function cclass_type_name( obj )
 	local t = type(obj)
-	print("cclass_type_name: t= "..t)
+	-- print("cclass_type_name: t= "..t)
 	if t~="table" and t~="userdata" then return t end
 	local mt = getmetatable(obj)
-	print(mt)
-	print(mt.classname_)
+	-- print(mt)
+	-- print(mt.classname_)
 	if mt and mt.classname_ then
 		return mt.classname_
 	else
@@ -213,7 +286,7 @@ local function CClass(classname, namespace)
 		mt = {}
 	end
 	mt.namespace_string = namespace.cclass_namespace_name
-	dump(mt)
+	-- dump(mt)
 
 	mt.__index = cclass_mt_index
 
