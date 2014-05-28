8d6f13adfd8d8887c20a605993bc6d165c4fa0ea
SunLightJuly <ygqy8181@qq.com>
2014-05-28 09:20:25 +0800

    stop test ffi

--- hello223/scripts/ffi_test.lua
+++ hello223/scripts/ffi_test.lua
@@ -6,15 +6,23 @@ print("==============================test ffi 1", ffi)
 
 ffi.cdef[[
 //int luabinderload(const char * classname);
-//int test_add_xy(int x, int y);
+void test_add_xy(int x, int y);
 int printf(const char*, ...);
+
+typedef void (*Func1) (int, int);
+
+//Func1 f = test_add_xy;
+//Func1 funcs[] = {test_add_xy, NULL};
 ]]
 
 print("==============================test ffi 2", ffi)
 
 local rni = ffi.C.printf("Hello %s!\n", "world")
--- local rni = ffi.C.luabinderload("CCNodeTest")
--- local rni = ffi.C.test_add_xy(26, 24)
 print("rni···:", rni)
+-- local rni = ffi.C.luabinderload("CCNodeTest")
+-- rni = ffi.C.test_add_xy(26, 24)
+-- rni = f(26, 24)
+-- rni = ffi.C.funcs[1](26, 24)
+-- print("rni···:", rni)
 
 return ffi
